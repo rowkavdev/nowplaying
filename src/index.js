fe462692035936b5cc5b6189da6662564fa1fab8
rowan kavanagh <rowkav0809@highgateschool.org.uk>
2026-09-21 00:00:15 +0100

    settings: export public configuration API
    Expose defaults, validation and immutable settings creation from the stable package entrypoint.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 export { renderCard } from "./card.js";
 export { createPresence } from "./presence.js";
 export { defineProvider } from "./provider.js";
+export { defaultSettings, validateSettings, createSettings } from "./settings.js";
 export { createPlexProvider } from "./providers/plex.js";
 export { createJellyfinProvider } from "./providers/jellyfin.js";
 export { createNavidromeProvider } from "./providers/navidrome.js";
