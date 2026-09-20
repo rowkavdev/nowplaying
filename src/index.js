78bb41cbb50ff79efcc3b28c69a8dd68e628f3b5
rowan kavanagh <rowkav0809@highgateschool.org.uk>
2026-09-21 00:04:38 +0100

    settings: export template API
    Expose supported fields, value construction, validation and formatting through the stable entrypoint.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ export { renderCard } from "./card.js";
 export { createPresence } from "./presence.js";
 export { defineProvider } from "./provider.js";
 export { defaultSettings, validateSettings, createSettings } from "./settings.js";
+export { templateFields, createTemplateValues, validateTemplate, formatTemplate } from "./template.js";
 export { createPlexProvider } from "./providers/plex.js";
 export { createJellyfinProvider } from "./providers/jellyfin.js";
 export { createNavidromeProvider } from "./providers/navidrome.js";
