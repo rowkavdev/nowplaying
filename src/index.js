b31e001a290ebb66bb34a4dc2e1bd2925dbfcfff
rowan kavanagh <rowkav0809@highgateschool.org.uk>
2026-09-21 05:15:24 +0100

    artwork: export default sanitizer runtime
    Expose the locked sharp-backed sanitizer from the package entry point.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 
 
+
 export { renderCard, cardThemes, resolveCardTheme } from "./card.js";
 export { createPresence } from "./presence.js";
 export { defineProvider } from "./provider.js";
@@ -16,6 +17,7 @@ export { createArtworkCache, artworkCacheKey } from "./artwork-cache.js";
 export { fetchArtwork, artworkDataUri } from "./artwork-fetch.js";
 export { createArtworkService } from "./artwork-service.js";
 export { createSharpArtworkSanitizer } from "./artwork-sanitizer.js";
+export { createDefaultArtworkSanitizer } from "./artwork-sanitizer-runtime.js";
 export { createPlexProvider } from "./providers/plex.js";
 export { createJellyfinProvider } from "./providers/jellyfin.js";
 export { createNavidromeProvider } from "./providers/navidrome.js";
